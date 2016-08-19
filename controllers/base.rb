e49c9b783ef15428c66960f18b98882fdd492d6b
John-Lin <linton.tw@gmail.com>
2016-08-19 15:50:26 +0800

    env variable with username

--- controllers/base.rb
+++ controllers/base.rb
@@ -15,7 +15,7 @@ class OpenKoreAPI < Sinatra::Base
 
     def authorized?
       @auth ||=  Rack::Auth::Basic::Request.new(request.env)
-      @auth.provided? and @auth.basic? and @auth.credentials and @auth.credentials == ['admin', ENV['PASSWORD']]
+      @auth.provided? and @auth.basic? and @auth.credentials and @auth.credentials == [ENV['USERNAME'], ENV['PASSWORD']]
     end
   end
 
