9402b28c1ce89995de43d5aa44bb2f0b46e74964
John-Lin <linton.tw@gmail.com>
2016-08-24 03:17:40 +0800

    config stuff

--- config/environments.rb
+++ config/environments.rb
@@ -8,6 +8,12 @@ configure :test do
   ENV['DATABASE_URL'] = 'sqlite://db/test.db'
 end
 
+configure :development, :test do
+  require 'config_env'
+  ConfigEnv.path_to_config("#{__dir__}/config_env.rb")
+end
+
+
 configure :production do
   # ENV['DATABASE_URL'] should be set by Heroku
   # Configuration settings should be directly set on Heroku
