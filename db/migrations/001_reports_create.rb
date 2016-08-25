9fd72c2d9e1902622d9008b12174d332fab60bed
John-Lin <linton.tw@gmail.com>
2016-08-25 12:41:21 +0800

    Encode None string

--- db/migrations/001_reports_create.rb
+++ db/migrations/001_reports_create.rb
@@ -5,9 +5,9 @@ Sequel.migration do
     create_table(:reports) do
       primary_key :id
       String :username, unique: true
-      String :char_info, default: 'None'
-      String :exp_report, default: 'None'
-      String :items, default: 'None'
+      String :char_info, default: 'Tm9uZQ=='
+      String :exp_report, default: 'Tm9uZQ=='
+      String :items, default: 'Tm9uZQ=='
       DateTime :created_at
       DateTime :updated_at
     end
